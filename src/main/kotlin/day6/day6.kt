8d0fe90177a9d57479f5d551f7a5c3d9231a28d4
koeltv <Valentin.koeltgen@utt.fr>
2022-12-07 21:39:37 +0100

    Optimized day6 (using characters on the go, no big buffer used)

--- src/main/kotlin/day6/day6.kt
+++ src/main/kotlin/day6/day6.kt
@@ -3,16 +3,21 @@ package day6
 import java.io.File
 
 fun main() {
-    val input = File("./resources/day6").readText()
+    val reader = File("./resources/day6").reader()
+
     var buffer = mutableListOf<Char>()
     val markerCount = 14 //Replace by 4 for the first part
-    for (i in 0 until markerCount-1) buffer.add(input[i])
-    for (i in markerCount-1..input.lastIndex) {
-        buffer.add(input[i])
-        if (buffer.none { c -> buffer.count { c2 -> c2 == c } > 1 }) {
-            println("first marker after character ${i+1}")
-            return
+
+    var i = 0
+    while (reader.ready()) {
+        buffer.add(reader.read().toChar())
+        if (i >= markerCount - 1) {
+            if (buffer.none { c -> buffer.count { c2 -> c2 == c } > 1 }) {
+                println("first marker after character ${i + 1}")
+                return
+            }
+            buffer = buffer.drop(1).toMutableList()
         }
-        buffer = buffer.toList().drop(1).toMutableList()
+        i++
     }
 }
